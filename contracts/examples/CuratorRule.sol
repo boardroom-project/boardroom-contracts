0545429226a5620f1eaaf502491b40979555e905
Nick Dodson <silentcicero@outlook.com>
2016-10-20 12:12:13 -0500

    Update CuratorRule.sol

--- contracts/examples/CuratorRule.sol
+++ contracts/examples/CuratorRule.sol
@@ -5,35 +5,54 @@ import "BoardRoom.sol";
 contract CuratorRule is Rules {
     function CuratorRule (address _registry){
         registry = OpenRegistry(_registry);
+        owner = msg.sender;
     }
 
     function hasWon(uint _proposalID) constant returns (bool) {
-        BoardRoom board = BoardRoom(msg.sender);
         uint nay = board.positionWeightOf(_proposalID, 0);
         uint yea = board.positionWeightOf(_proposalID, 1);
         uint totalVoters = board.numVoters(_proposalID);
+        
         for(uint i = 0; i < curators.length; i++){
           var (position, weight, created) = board.voteOf(_proposalID,curators[i]);
-          if (position == 0){
+          if (position == 0 && isCurator[_proposalID,curators[i]] == true){
             return false;
           }
         }
+        
         if(totalVoters > 0 && yea > nay ) {
             return true;
         }
+        
         return false;
+    }
+    
+    function setup(address _board) {
+        if (msg.sender == owner) {
+            board = _board;
+        }
     }
 
     function addCurator (address _curator){
-        BoardRoom board = BoardRoom(msg.sender);
-        if (registry.isMember(_curator)){
+        if (msg.sender == address(board)){
           curators.push(_curator);
           isCurator[_curator] = true;
         }
     }
+    
+    function removeCurator (address _curator) {
+        if (msg.sender == address(board)){
+            isCurator[_curator] = false;
+            
+            for(uint i = 0; i < curators.length; i++){
+                if (curators[i] == _curator) {
+                    delete curators[i];
+                }
+            }
+        }
+    }
 
     function canVote(address _sender, uint _proposalID) constant returns (bool) {
-        BoardRoom board = BoardRoom(msg.sender);
         var (name, destination, proxy, value, validityHash, executed, debatePeriod, created) = board.proposals(_proposalID);
         if(registry.isMember(_sender) && now < created + debatePeriod) {
             return true;
@@ -49,6 +68,9 @@ contract CuratorRule is Rules {
     function votingWeightOf(address _sender, uint _proposalID) constant returns (uint) {
         return 1;
     }
+    
+    address owner;
+    address board;
     OpenRegistry public registry;
     address[] curators;
     mapping(address => bool) public isCurator;
