8e4b4c4d864e865e5e9dd1ce3d3e7e4d19f94d22
Bill Gleim <bgleim@gmail.com>
2017-02-07 13:04:20 -0800

    Add multi choice ruleset placeholder

---
+++ contracts/examples/MultiChoiceRules.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.4.3;
+
+import "Rules.sol";
+import "BoardRoom.sol";
+
+contract MultiChoiceRules is Rules {
+  function hasWon(uint _proposalID) public constant returns (bool) {
+    BoardRoom board = BoardRoom(msg.sender);
+    uint nay = board.positionWeightOf(_proposalID, 0);
+    uint yea = board.positionWeightOf(_proposalID, 1);
+
+    if(yea > nay) {
+      return true;
+    }
+  }
+
+  function canVote(address _sender, uint _proposalID) public constant returns (bool) {
+    return true;
+  }
+
+  function canPropose(address _sender) public constant returns (bool) {
+    return true;
+  }
+
+  function votingWeightOf(address _sender, uint _proposalID) public constant returns (uint) {
+    return 1;
+  }
+}
